69e8eed1383e4db77706ab77404310078ffc35c9
Maxim Abakumov <maximnatural@gmail.com>
2020-07-18 23:35:41 +0300

    замечания в комментариях

--- Course2Week5Task1/FirstViewController.swift
+++ Course2Week5Task1/FirstViewController.swift
@@ -34,6 +34,8 @@ class FirstViewController: UIViewController {
         appearanceTimeLabel.text = getDateAndTime()
     }
     
+    
+    // чем тебя не устраивает viewDidAppear для отображения обновления времени? два раза вызывается метод по одному и тому же поводу
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
@@ -41,6 +43,7 @@ class FirstViewController: UIViewController {
     }
     
     @IBAction func tapUpdateButton(_ sender: UIBarButtonItem) {
+        // это табу - методы жизненного цикла контроллера не вызываются напрямую, это нарушение LSP (SOLID - принцип Барбары Лисков), делай свой приватный метод и вызывай его по кнопке; рекомендую почитать доку Apple про методы Layout (viewWillLayout... , viewDidLayout...) 
         viewWillLayoutSubviews()
     }
 }
